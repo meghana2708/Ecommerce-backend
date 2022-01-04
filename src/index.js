35b2c1110daf730479bf6916a88c5890e01d99a4
Sai Meghana Vemula <meghana.bamr@gmail.com>
2022-01-04 21:53:11 +0530

    config changes

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ app.use(express.json())
 app.use("/api/v1/auth", authRoutes)
 app.use('/api/v1/category',categoryRoutes)
 app.use('/api/v1/product',productRoutes)
+console.log("hi")
 app.listen(port,(req,res) => {
   console.log(`server listening at port ${port}`)
 })
